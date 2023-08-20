5e934da1dd291fcf934dc1582bfe57577d247b9f
Toritoriri <kuligina.vika@bk.ru>
2023-08-20 13:04:42 +0500

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let openBr = {};
+  bracketsConfig.forEach( items => {
+    openBr[items[0]] = items[1];});
+
+
+    function checkBrac(str) {
+      const result = [];
+      for (let i = 0; i < str.length; i++) {
+          let currentItem = str[i];
+          const a = result[result.length - 1];
+
+          if (result.length && openBr[a] === currentItem) {
+              result.pop();
+              continue;
+          }
+
+          result.push(currentItem);
+      }
+      return result.length === 0;
+  }
+  return checkBrac(str);
 }
